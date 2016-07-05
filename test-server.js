7d952bac2ebacc362db4caa2fed0aabc74484712
Byoung-cheol Yang <lambdavc@protonmail.ch>
2016-07-05 14:57:45 +0900

    npm start

--- test-server.js
+++ test-server.js
@@ -1,2 +1,11 @@
-console.log('test-server');
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+var portNumber = 3000;
+
+app.listen(portNumber, function(){
+    console.log('test server listening on port ' + portNumber);
+});
 
